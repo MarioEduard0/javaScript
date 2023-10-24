2ce13f515a44152ec1ab346b97d4cc8b9abc9210
Mario Eduardo <marioeduardo251@gmail.com>
2023-10-24 13:56:03 +0200

    acctive class poke card started

--- PokeApi/script.js
+++ PokeApi/script.js
@@ -1,23 +1,54 @@
 const toggleButton = document.getElementById('toggle-types');
 const typesContainer = document.querySelector('.types');
+const divsInsideTypes = typesContainer.querySelectorAll('div');
 
-let isTypesVisible = false; // Variable para controlar si los tipos están visibles o no
+var textArray = [];
+
+let isTypesVisible = false;
 
 toggleButton.addEventListener('click', () => {
     if (!isTypesVisible) {
-        typesContainer.style.display = 'flex'; // Muestra los tipos
+        typesContainer.style.display = 'flex';
         setTimeout(() => {
-            typesContainer.style.opacity = 1; // Cambia la opacidad a 1 después de 1 segundo
-            typesContainer.style.top = '0'; // Cambia la posición top a 0 después de 1 segundo
-        }, 10); // Un pequeño retraso para asegurar la animación
+            typesContainer.style.opacity = 1;
+            typesContainer.style.top = '0';
+        }, 100);
     } else {
-        typesContainer.style.opacity = 0; // Cambia la opacidad a 0
-        typesContainer.style.top = '-50px'; // Cambia la posición top a -50px
+        typesContainer.style.opacity = 0;
+        typesContainer.style.top = '-50px';
         setTimeout(() => {
-            typesContainer.style.display = 'none'; // Oculta los tipos después de la animación
-        }, 1000); // La animación dura 1 segundo
+            typesContainer.style.display = 'none';
+        }, 1000);
     }
-    isTypesVisible = !isTypesVisible; // Invierte el estado de visibilidad
+    isTypesVisible = !isTypesVisible;
 
-    toggleButton.classList.toggle('active'); // Cambia la clase del botón para activar/desactivar la rotación 3D
+    toggleButton.classList.toggle('active');
+
+    // Elimina la clase "active" de todos los otros divs
+    divsInsideTypes.forEach(div => {
+        div.classList.remove('active');
+    });
+
+    textArray = [];
+});
+
+
+divsInsideTypes.forEach(div => {
+    div.addEventListener('click', () => {
+        const pElement = div.querySelector('p');
+        const pText = pElement.textContent;
+
+        const index = textArray.indexOf(pText);
+
+        if (index !== -1) {
+            textArray.splice(index, 1); // Elimina el elemento del array si ya está presente
+            div.classList.remove('active'); // Quita la clase "active" del div
+        } else {
+            textArray.push(pText);
+            div.classList.add('active'); // Agrega la clase "active" al div
+        }
+
+        console.log('Texto actual del <p>:', pText);
+        console.log('Array de textos:', textArray);
+    });
 });
